e31479bf70e54379e2a8bfecf0fb61f4865ef29e
Rizki Rinaldi <rizkirenaldi115@gmail.com>
2026-08-10 18:10:44 +0700

    menambahkan fitur reset warna

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const circles = document.querySelectorAll(".circle");
+const resetButton = document.getElementById("resetButton");
+
+// menyimpan warna default body
+const defaultColor = getComputedStyle(document.body).backgroundColor;
+
 circles.forEach((circle) => {
   circle.addEventListener("click", ({ target }) => {
     const { backgroundColor } = getComputedStyle(target);
     document.body.style.backgroundColor = backgroundColor;
   });
+});
+
+// Tombol Reset
+resetButton.addEventListener("click", () => {
+  document.body.style.backgroundColor = defaultColor;
 });
